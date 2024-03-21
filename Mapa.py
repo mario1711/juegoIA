a0b15aa7e773e9e4f89478eced229937453f0cf7
Mario Hernandez <marioahm1711@gmail.com>
2024-03-20 22:55:53 -0600

    cost validation
    Changed the cordinates in map representation

--- Mapa.py
+++ Mapa.py
@@ -14,6 +14,7 @@ class Map:
             1: pygame.image.load(os.path.join(self.sprite_dir, "ground.png")).convert_alpha(),
             2: pygame.image.load(os.path.join(self.sprite_dir, "spawn.png")).convert_alpha(),
             3: pygame.image.load(os.path.join(self.sprite_dir, "sand.png")).convert_alpha(),
+            4: pygame.image.load(os.path.join(self.sprite_dir, "lava.png")).convert_alpha()
 
         }
 
@@ -36,6 +37,7 @@ class Map:
         ]
 
         self.generate_sand()
+        self.generate_lava()
 
     def generate_sand(self):
         # Definir un área para el lago de lava
@@ -52,6 +54,21 @@ class Map:
                     if random.random() < 0.5:
                         self.map[y][x] = 3
 
+    def generate_lava(self):
+        # Definir un área para el lago de lava
+        lake_size = 4
+        lake_center = (random.randint(lake_size // 2, MAP_SIZE - lake_size // 2 - 1),
+                       random.randint(lake_size // 2, MAP_SIZE - lake_size // 2 - 1))
+
+        # Generar el lago de lava adyacente
+        for y in range(1, MAP_SIZE - 1):
+            for x in range(1, MAP_SIZE - 1):
+                dx = x - lake_center[0]
+                dy = y - lake_center[1]
+                if abs(dx) <= lake_size // 2 and abs(dy) <= lake_size // 2:
+                    if random.random() < 0.5:
+                        self.map[y][x] = 4
+
     def draw(self, screen):
         for row_index, row in enumerate(self.map):
             for col_index, tile_type in enumerate(row):
@@ -60,7 +77,7 @@ class Map:
                     screen.blit(tile, (col_index * TILE_SIZE, row_index * TILE_SIZE))
 
     def get_tile_cost(self, x, y):
-        pos = self.map[x][y]
+        pos = self.map[y][x]
         if pos == 0:  # Borde - No transitable
             return float('inf')
         elif pos == 1:  # Tierra - Bajo costo
@@ -68,8 +85,8 @@ class Map:
         elif pos == 2:  # Punto de inicio - Bajo costo
             return 1
         elif pos == 3:  # Lava - Alto costo
-            return 100
-        elif pos == 4:  # Arena - Costo medio
             return 5
+        elif pos == 4:  # Arena - Costo medio
+            return 100
         else:  # Otros valores - No transitable
             return float('inf')
